9fa26048e7edc34959255ba726ac3568a2fea40f
theresatiffanytan <108322740+theresatiffanytan@users.noreply.github.com>
2023-07-15 21:54:14 +0700

    Add files via upload

---
+++ Nano3_MRTStation/Nano3_MRTStation/Nano3_MRTStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nano3_MRTStationApp.swift
+//  Nano3_MRTStation
+//
+//  Created by Theresa Tiffany on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano3_MRTStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

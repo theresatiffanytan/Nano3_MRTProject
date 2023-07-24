f65c83bca5507c159fb83a5209dac044bb69af10
- <->
2023-07-24 09:51:52 +0700

    -Updated WatchCompassView

--- Nano3_MRTStation/Nano3 MRT Watch App/WatchCompassView.swift
+++ Nano3_MRTStation/Nano3 MRT Watch App/WatchCompassView.swift
@@ -41,52 +41,57 @@ struct WatchCompassView: View {
     }
     
     var content: some View {
-        VStack {
-            
-            Spacer()
-            ScrollView{
-                Text(locationManager.getTripActionDesc())
-                    .font(.system(size: 14))
+        GeometryReader { geometry in
+            ZStack {
+                
+                ScrollView{
+                    Text(locationManager.getTripActionDesc())
+                        .font(.system(size: 14))
                     
-             
-            }.frame(width: 160, height: 38)
-            Spacer()
-            Image("compass")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100, height: 100)
-                .rotationEffect(.degrees(locationManager.heading))
-                .overlay(alignment: .top) {
-                    Image("radial")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                }
-                .overlay {
-                    Image(systemName: "location.north.fill")
-                        .resizable()
-                        .foregroundColor(.accentColor)
-                        .frame(width: 20, height: 20)
-                        .rotationEffect(.degrees(locationManager.heading))
-                }
-            Spacer()
-        
+                    
+                }.frame(width: 160, height: 38/2)
+                    .position(x: geometry.size.width / 2 , y: geometry.size.height * 0.1)
+               
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                  
+                    .rotationEffect(.degrees(locationManager.heading))
+                    .overlay(alignment: .top) {
+                        Image("radial")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .overlay {
+                        Image(systemName: "location.north.fill")
+                            .resizable()
+                            .foregroundColor(.accentColor)
+                            .frame(width: 20, height: 20)
+                            .rotationEffect(.degrees(locationManager.heading))
+                    }
+                    .position(x: geometry.size.width / 2 , y: geometry.size.height * 0.5)
+               
+                
                 Text("\(locationManager.distance.distanceDesc) away")
                     .font(.system(size: 12))
                 ScrollView{
                     Text("\(locationManager.headingDesc)")
                         .font(.system(size: 14))
                 }.frame(width: 160, height: 38)
+                    .position(x: geometry.size.width / 2 , y: geometry.size.height * 0.93)
+                
+                
+                
+            }
+            .multilineTextAlignment(.center)
+            .onDisappear{
+                WatchVM.receivedDestionation.removeAll()
+            }
             
-            Spacer()
             
         }
-        .multilineTextAlignment(.center)
-        .onDisappear{
-            WatchVM.receivedDestionation.removeAll()
-        }
-       
-        
     }
 }
 
